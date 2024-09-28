2878a59c4a530c252388cb985c5fc9f9527b13ff
saniya-kri <anjali45528@gmail.com>
2024-09-28 22:46:06 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,15 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// module.exports = {
+//         webpack: (config, { dev }) => {
+//           if (dev) {
+//             config.watchOptions = {
+//               poll: 1000,
+//               aggregateTimeout: 300,
+//             };
+//           }
+//           return config;
+//         },
+//       };
